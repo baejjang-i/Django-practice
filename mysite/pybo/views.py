981a27cd99cfccf51cbba9d4b2ae346abb06a367
baejjang_i <baejjang_i@kakao.com>
2021-11-11 23:02:17 +0900

    3일 차 연습

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
-
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("pybo 페이지")
+    """
+    pybo list print
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list' : question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo content print
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
