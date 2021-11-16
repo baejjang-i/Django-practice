6342021a715a6e79e94669092bcda00b0522f8cf
이은창 <baejjang_i@ieunchangs-MacBook-Air.local>
2021-11-16 23:15:32 +0900

    4일 차 연습

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
+from django.utils import timezone
+
+
 # Create your views here.
 
 def index(request):
@@ -8,8 +11,9 @@ def index(request):
     pybo list print
     """
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list' : question_list}
+    context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
+
 
 def detail(request, question_id):
     """
@@ -18,3 +22,13 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    """
+    pybo answer registration
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'),
+                               create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
